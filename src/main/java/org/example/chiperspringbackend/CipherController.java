8f65ffaa592cef89e0458f97cf902b5ad03f14c7
Yoe <dembouz719@gmail.com>
2024-11-04 19:43:37 +0100

    update name to cipher

--- src/main/java/org/example/chiperspringbackend/CipherController.java
+++ src/main/java/org/example/chiperspringbackend/CipherController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/chiper")
+@RequestMapping("/api/cipher")
 public class CipherController{
 
     Cipher cipher = new Cipher(3);
--- src/main/java/org/example/chiperspringbackend/CipherSpringBackendApplication.java
+++ src/main/java/org/example/chiperspringbackend/CipherSpringBackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChiperSpringBackendApplication {
+public class CipherSpringBackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ChiperSpringBackendApplication.class, args);
+        SpringApplication.run(CipherSpringBackendApplication.class, args);
     }
 
 }
