741ecb018b83373958d855e0c564debf157fd312
Yoe <dembouz719@gmail.com>
2024-11-04 15:09:26 +0100

    Created yml file

--- src/test/java/org/example/chiperspringbackend/ChiperSpringBackendApplicationTests.java
+++ src/test/java/org/example/chiperspringbackend/ChiperSpringBackendApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ChiperSpringBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
