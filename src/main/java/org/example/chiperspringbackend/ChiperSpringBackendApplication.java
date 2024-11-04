b5c318903faa9856d8c56d20d91741e590375720
Yoe <dembouz719@gmail.com>
2024-11-04 15:05:12 +0100

    Initial commit

---
+++ src/main/java/org/example/chiperspringbackend/ChiperSpringBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.chiperspringbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChiperSpringBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChiperSpringBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/chiperspringbackend/ChiperSpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.chiperspringbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChiperSpringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
