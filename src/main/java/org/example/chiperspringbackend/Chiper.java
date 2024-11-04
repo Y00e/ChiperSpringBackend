439d6a53a197a4aa92d22724a6fd3e0c11f1553b
Yoe <dembouz719@gmail.com>
2024-11-04 17:21:44 +0100

    Created decrypt password method

--- src/main/java/org/example/chiperspringbackend/Chiper.java
+++ src/main/java/org/example/chiperspringbackend/Chiper.java
@@ -12,7 +12,7 @@ public class Chiper {
 
     // Method to encrypt a Password
     public String encryptPassword(String password) {
-        // TODO Implement encryption logic
+        // encryption logic
         if (password.contains(" ")) {
             throw new IllegalArgumentException("Password cannot contain spaces.");
         }
@@ -34,6 +34,16 @@ public class Chiper {
     // Method to decrypt a Password
     public String decryptPassword(String password) {
         // TODO Implement decryption logic
-        return password;
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < password.length(); i++) {
+            char c = password.charAt(i);
+            if (c >= 'a' && c <= 'z') {
+                c = (char) ((c - 'a' - shift + 26) % 26 + 'a');
+            } else if (c >= 'A' && c <= 'Z') {
+                c = (char) ((c - 'A' - shift + 26) % 26 + 'A');
+            }
+            result.append(c);
+        }
+        return result.toString();
     }
 }
