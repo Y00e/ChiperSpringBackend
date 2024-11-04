6c84315011e9593df6eca1caba2f20226e1fe582
Yoe <dembouz719@gmail.com>
2024-11-04 18:05:36 +0100

    created endpoints for encrpyt and decrypt

--- src/main/java/org/example/chiperspringbackend/ChiperController.java
+++ src/main/java/org/example/chiperspringbackend/ChiperController.java
@@ -1,5 +1,28 @@
 package org.example.chiperspringbackend;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/chiper")
 public class ChiperController {
+
+    private final Chiper chiper;
+
+    public ChiperController(Chiper chiper) {
+        this.chiper = chiper;
+    }
+
+    @PostMapping("encrypt")
+    public String encrypt(@RequestBody String message) {
+        return chiper.encryptPassword(message);
+    }
+
+    @PostMapping("decrypt")
+    public String decrypt(@RequestBody String encryptedMessage) {
+        return chiper.decryptPassword(encryptedMessage);
+    }
 
 }
