004c65185b0a17b3fc9bc7d071ee7c8d8f12a4c9
Yoe <dembouz719@gmail.com>
2024-11-05 20:54:53 +0100

    changed variable name password to input

--- src/main/java/org/example/chiperspringbackend/Cipher.java
+++ src/main/java/org/example/chiperspringbackend/Cipher.java
@@ -11,15 +11,15 @@ public class Cipher {
     }
 
     // Method to encrypt a Password
-    public String encrypt(String password) {
+    public String encrypt(String input) {
         // encryption logic
-        if (password.contains(" ")) {
+        if (input.contains(" ")) {
             throw new IllegalArgumentException("Password cannot contain spaces.");
         }
 
         StringBuilder result = new StringBuilder();
-        for (int i = 0; i < password.length(); i++) {
-            char c = password.charAt(i);
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
             if (c >= 'a' && c <= 'z') {
                 c = (char) ((c - 'a' + shift) % 26 + 'a');
             } else if (c >= 'A' && c <= 'Z') {
@@ -32,11 +32,11 @@ public class Cipher {
     }
 
     // Method to decrypt a Password
-    public String decrypt(String password) {
+    public String decrypt(String input) {
         // TODO Implement decryption logic
         StringBuilder result = new StringBuilder();
-        for (int i = 0; i < password.length(); i++) {
-            char c = password.charAt(i);
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
             if (c >= 'a' && c <= 'z') {
                 c = (char) ((c - 'a' - shift + 26) % 26 + 'a');
             } else if (c >= 'A' && c <= 'Z') {
--- src/main/java/org/example/chiperspringbackend/CipherController.java
+++ src/main/java/org/example/chiperspringbackend/CipherController.java
@@ -1,10 +1,7 @@
 package org.example.chiperspringbackend;
 
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/cipher")
@@ -12,15 +9,18 @@ public class CipherController{
 
     Cipher cipher = new Cipher(3);
 
-
+    @CrossOrigin(origins = "http://localhost:5173")
     @PostMapping("encrypt")
-    public String encrypt(@RequestBody String password) {
-        return cipher.encrypt(password);
+    public String encrypt(@RequestBody String input) {
+        return cipher.encrypt(input);
     }
 
+    @CrossOrigin(origins = "http://localhost:5173")
     @PostMapping("decrypt")
-    public String decrypt(@RequestBody String encryptedpassword) {
-        return cipher.decrypt(encryptedpassword);
+    public String decrypt(@RequestBody String encryptedInput) {
+        return cipher.decrypt(encryptedInput);
     }
+
+
 
 }
