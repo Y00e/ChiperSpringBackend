93c66585c118eab53f7ed6200cab6ea59c5c540c
Yoe <dembouz719@gmail.com>
2024-11-07 12:52:48 +0100

    Added Origins for GitHub

--- src/main/java/org/example/chiperspringbackend/CipherController.java
+++ src/main/java/org/example/chiperspringbackend/CipherController.java
@@ -3,20 +3,20 @@ package org.example.chiperspringbackend;
 
 import org.springframework.web.bind.annotation.*;
 
-
+@CrossOrigin(origins = {"http://localhost:5173","https://y00e.github.io"})
 @RestController
 @RequestMapping("/api/cipher")
 public class CipherController{
 
     Cipher cipher = new Cipher(3);
 
-    @CrossOrigin(origins = {"http://localhost:5173","https://y00e.github.io"})
+
     @PostMapping("encrypt")
     public String encrypt(@RequestParam String input) {
        return cipher.encrypt(input);
     }
 
-    @CrossOrigin(origins = {"http://localhost:5173","https://y00e.github.io"})
+
     @PostMapping("decrypt")
     public String decrypt(@RequestParam String encryptedInput) {
         return cipher.decrypt(encryptedInput);
